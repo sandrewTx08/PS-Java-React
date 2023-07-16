69bcbfb03a33525353be98226c3b1a15aef83941
sandrewtx08 <sandrewtx08@protonmail.com>
2023-07-16 14:30:14 -0300

    perf: index conta

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,10 @@ CREATE TABLE conta
     id_conta SERIAL NOT NULL PRIMARY KEY,
     nome_responsavel VARCHAR(50) NOT NULL
 );
+CREATE INDEX id_conta_index ON conta (id_conta);
 
+INSERT INTO conta (id_conta, nome_responsavel) VALUES (1,'Fulano');
+INSERT INTO conta (id_conta, nome_responsavel) VALUES (2,'Sicrano');
 
 CREATE TABLE transferencia
 (
@@ -18,9 +21,7 @@ CREATE TABLE transferencia
         FOREIGN KEY (conta_id)
         REFERENCES conta(id_conta)
 );
-
-INSERT INTO conta (id_conta, nome_responsavel) VALUES (1,'Fulano');
-INSERT INTO conta (id_conta, nome_responsavel) VALUES (2,'Sicrano');
+CREATE INDEX conta_id_index ON transferencia (conta_id);
 
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (1,'2019-01-01 12:00:00+03',30895.46,'DEPOSITO', null,1);
 INSERT INTO transferencia (id,data_transferencia, valor, tipo, nome_operador_transacao, conta_id) VALUES (2,'2019-02-03 09:53:27+03',12.24,'DEPOSITO', null,2);
